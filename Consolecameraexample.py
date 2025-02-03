dc157ceb18d5237bc567047d488bbe6fb42942d9
Jynoqtra <Transfinityofficial@gmail.com>
2025-02-03 22:07:05 +0300

    Create Consolecameraexample.py

---
+++ Consolecameraexample.py
@@ -0,0 +1,16 @@
+from JynPopMod import ConsoleCam
+
+cam = ConsoleCam.start()  # Starting to record.
+
+print("Hello, JynPopMod!")    # Printing while Camera is recording.
+print("This output is being captured.")
+
+captured_output = cam.get_new() # Saving the record.
+
+cam.stop()   # Stopping capturing.
+
+print(captured_output)  # Printing captured output.   U dont have to print it u can use it for data like checking for a spesific message in console.
+
+print("This will not be in the captured output.")   # This won't be captured
+
+# Be cool 🧊 no not that type.That type:😎
